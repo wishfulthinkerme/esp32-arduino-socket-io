aeae1671e587273e32a837d7920d60417d9ab759
Mateusz Kasprzak <kasprzak.mat@gmail.com>
2018-03-01 13:18:49 +0100

    Change buttons

--- front/src/components/ButtonGroup.js
+++ front/src/components/ButtonGroup.js
@@ -3,11 +3,9 @@ import './button-group.css';
 
 export default ({ onSend }) => (
   <div className="button-group">
-    <div className="button-group__btn" onClick={onSend('toRadio', 'R01:LIG:01')}>R01:LIG:01</div>
-    <div className="button-group__btn" onClick={onSend('toRadio', 'R01:LIG:00')}>R01:LIG:00</div>
-    <div className="button-group__btn" onClick={onSend('toRadio', 'R02:STP:15')}>R02:STP:15</div>
-    <div className="button-group__btn" onClick={onSend('toRadio', 'R03:STP:15')}>R03:STP:15</div>
-    <div className="button-group__btn" onClick={onSend('toRadio', 'R04:STP:15')}>R04:STP:15</div>
-    <div className="button-group__btn" onClick={onSend('toRadio', 'R05:STP:15')}>R05:STP:15</div>
+    <div className="button-group__btn" onClick={onSend('toRadio', 'R01:LIG:01')}>R01 - Light on</div>
+    <div className="button-group__btn" onClick={onSend('toRadio', 'R01:LIG:00')}>R01 - Light off</div>
+    <div className="button-group__btn" onClick={onSend('toRadio', 'R02:STP:15')}>R02 - Check Distance</div>
+    <div className="button-group__btn" onClick={onSend('toRadio', 'R05:STP:15')}>R05 - Check temp</div>
   </div>
 );
