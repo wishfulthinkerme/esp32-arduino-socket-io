8f9097cc8ec9149c3f896a9d7719b5e63422bb0b
Mateusz Kasprzak <kasprzak.mat@gmail.com>
2018-03-01 13:11:07 +0100

    Remove transmitting from light sensor

--- arduino/receiver-light/receiver.ino
+++ arduino/receiver-light/receiver.ino
@@ -5,14 +5,10 @@
 RF24 radio(9, 10);
 
 const byte rxAddr[6] = "00001";
-const byte txAddr[6] = "00002";
 const String myRadioKey = "R01";
 const int packageSize = 15;
 const int LED_YELLOW = 2; // D2
 const int LIGHT = A0;
-
-unsigned long previousMillis = 0;
-const long interval = 15000;
 
 void initRadio()
 {
@@ -22,7 +18,7 @@ void initRadio()
   radio.enableAckPayload();
   radio.enableDynamicPayloads();
   radio.openReadingPipe(0, rxAddr);
-  radio.openWritingPipe(txAddr);
+
   radio.startListening();
 }
 
@@ -42,24 +38,6 @@ void setup()
   initRadio();
 }
 
-void transmitData(String actionType, String actionValue, String actionDesc)
-{
-  radio.stopListening();
-  char package[packageSize] = {0};
-  String(myRadioKey + ":" + actionType + ":" + actionValue).toCharArray(package, packageSize);
-  bool sent = radio.write(&package, sizeof(package));
-  Serial.print(String(actionDesc + ": "));
-  Serial.println(package);
-  digitalWrite(LED_YELLOW, HIGH);
-  delay(10);
-  digitalWrite(LED_YELLOW, LOW);
-  if (sent)
-  {
-    Serial.println("Sent");
-  }
-  radio.startListening();
-}
-
 void receiveRadio()
 {
   char received[packageSize] = {};
@@ -67,8 +45,7 @@ void receiveRadio()
   String radioKey = String(received).substring(0, 3);
   String radioType = String(received).substring(4, 7);
   String radioVal = String(received).substring(8, 15);
-  Serial.println(radioKey);
-  Serial.println(radioType);
+
   if (radioKey != myRadioKey)
   {
     return;
@@ -90,13 +67,6 @@ void receiveRadio()
 
 void loop()
 {
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval)
-  {
-    previousMillis = currentMillis;
-  }
-
   if (radio.available())
   {
     receiveRadio();
