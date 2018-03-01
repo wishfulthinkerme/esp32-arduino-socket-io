28d6a25f4ac8a4379e6e092c727aeeaf1aabbc01
Roman Pylypets <roman.pylypets@hotmail.com>
2018-03-01 13:34:56 +0100

    Reverse key as well

--- front/src/components/HistoryTable.js
+++ front/src/components/HistoryTable.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import './table.css';
 
-export default ({ history }) => (
-  <table className="app-table table table-striped">
-    <thead>
-      <tr>
-        <th scope="col">Key</th>
-        <th scope="col">Data</th>
-      </tr>
-    </thead>
-    <tbody>
-      {history.slice().reverse().map((data, key) => (
-        <tr key={`${key}`}>
-          <td>{key}</td>
-          <td>{data}</td>
+export default ({ history }) => {
+  const reverseHistory = history.slice().reverse();
+  return (
+    <table className="app-table table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Key</th>
+          <th scope="col">Data</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {reverseHistory.map((data, key) => (
+          <tr key={`${key}`}>
+            <td>{reverseHistory.length - key}</td>
+            <td>{data}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
